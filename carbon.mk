0b4b0e8ce7f0d16da962f1dd27ca55a59af47ce9
bigshotrob22 <rrascon22@gmail.com>
2013-10-30 15:02:23 -0700

    Remove common_gsm.mk import
    Change-Id: I909f17caeeac658f77baf56efa7b63936098fe07

--- carbon.mk
+++ carbon.mk
@@ -1,8 +1,6 @@
 $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 
 # Inherit some common Carbon stuff.
-$(call inherit-product, vendor/carbon/config/common_gsm.mk)
-
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
